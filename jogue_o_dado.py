4e66d993b84e9ac5b8ea9d89dc4cdc49a09de81c
Heber vasconselos de souza <42897977+Heber9889@users.noreply.github.com>
2021-01-01 12:03:22 -0300

    Add files via upload
    Minigame

---
+++ jogue_o_dado.py
@@ -0,0 +1,29 @@
+import random 
+
+class SimuladorDeDado:
+    def __init__(self):
+        self.valor_minimo = 1
+        self.valor_maximo = 6
+        self.mensagem = "#-----------------------------------------------#\n|        Voçê Gostaria De Jogar o Dado?         |\n#-----------------------------------------------#\n     :"
+
+    def Iniciar(self):
+        resposta = input(self.mensagem)
+        try:        
+            if resposta == "sim" or resposta == "s":
+                self.GerarValorDoDado()
+            elif resposta == "nao" or resposta == "n":
+                print("#-----------------------------------------------#\n|            (: Obrigado Por Jogar :)           |\n#-----------------------------------------------#")
+            else:
+                print("#---------------------------------------------------------#\n| Ocoreu Um Erro Responda Com (sim) ou (nao) Obrigado!    |\n#---------------------------------------------------------#")
+        except:
+            print("Ocoreu Um Erro Ao Reseber A Sua Mensagem")
+            
+    def GerarValorDoDado(self):
+        print(random.randint(self.valor_minimo, self.valor_maximo))
+        
+simulador = SimuladorDeDado()
+simulador.Iniciar()
+
+                      
+            
+        
